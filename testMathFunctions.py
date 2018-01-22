10e80b777db13e46bfdf90cf587eff7720d5c204
james947 <jmuriuki947@gmail.com>
2018-01-23 00:41:49 +0300

    Add 2 and 6 tests

--- testMathFunctions.py
+++ testMathFunctions.py
@@ -1,15 +1,36 @@
 #import statements
 import unittest
-import MathFunctions
+import mathFunctions
 
-class Knownvalues(unittest.TestCase)
+class Knownvalues(unittest.TestCase):
 #test areaCircle() pi r ^2
     def test_areaCircle_for_10radius(self):    
         #get the results of the function
-        result = MathFunctions.areaCircle(10)
+        result = mathFunctions.areaCircle(10)
         #check for the expected ouput
         expected= 314.1592653589793
         self.assertEqual(expected, result)
 
+class Knowvlues(unittest.TestCase):
+     #test areaCircle() pi r ^2
+    def test_areaCircle_for_2radius(self):    
+        #get the results of the function
+        result = mathFunctions.areaCircle(2)
+        #check for the expected ouput
+        expected= 12.566370614359172
+        self.assertEqual(expected, result)
+
+
+class Knownvlues(unittest.TestCase):
+    #test areaCircle() pi r ^2
+    def test_areaCircle_for_6radius(self):    
+        #get the results of the function
+        result = mathFunctions.areaCircle(6)
+        #check for the expected ouput
+        expected=113.09733552923255
+        self.assertEqual(expected, result)
+
+
+#run test
 if __name__ == '__main__':
-    app.run()
+    unittest.main()
