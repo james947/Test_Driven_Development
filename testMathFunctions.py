fec49495fcc71b3a58f64c2a3cafb7d0569a7352
james947 <jmuriuki947@gmail.com>
2018-01-22 23:48:39 +0300

    Add area test function

--- testMathFunctions.py
+++ testMathFunctions.py
@@ -1,4 +1,12 @@
 #import statements
 import unittest
-import mathFunctions
+import MathFunctions
 
+class Knownvalues(unittest.TestCase)
+#test areaCircle() pi r ^2
+    def test_areaCircle_for_10radius(self):    
+        #get the results of the function
+        result = MathFunctions.areaCircle(10)
+        #check for the expected ouput
+        expected= 314.1592653589793
+        self.assertEqual(expected, result)
